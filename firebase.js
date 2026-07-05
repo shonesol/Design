498362fe265a1a01e79e9af841a5264767452270
TAMALE SHONE <70106850+shonesol@users.noreply.github.com>
2026-07-05 11:17:33 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,17 @@
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "AIzaSyA1wmHCfPLOmw1u3zUM-M2bUlCUWJDv2Bk",
+  authDomain: "design-a0e45.firebaseapp.com",
+  databaseURL: "https://design-a0e45-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "design-a0e45",
+  storageBucket: "design-a0e45.firebasestorage.app",
+  messagingSenderId: "752963168855",
+  appId: "1:752963168855:web:668b25771e5a85e4489112",
+  measurementId: "G-Y803FMNDL3"
 };
 
 const app = initializeApp(firebaseConfig);
