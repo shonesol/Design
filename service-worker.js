d8e94e9c90c6323b94c092433111aab64c220679
TAMALE SHONE <70106850+shonesol@users.noreply.github.com>
2026-07-08 10:43:38 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = "fashion-ai-v1";
+
+const files = [
+
+"index.html",
+"dashboard.html",
+"style.css",
+"firebase.js",
+"chat-ai.js",
+"upload.js"
+
+];
+
+
+self.addEventListener(
+"install",
+event=>{
+
+event.waitUntil(
+
+caches.open(CACHE_NAME)
+.then(cache=>{
+
+return cache.addAll(files);
+
+})
+
+);
+
+});
+
+
+
+self.addEventListener(
+"fetch",
+event=>{
+
+event.respondWith(
+
+caches.match(event.request)
+
+.then(response=>{
+
+return response || fetch(event.request);
+
+})
+
+);
+
+});
