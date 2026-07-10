1be1ecbe6e796b8419f178f06ab94aae8237c65e
TAMALE SHONE <70106850+shonesol@users.noreply.github.com>
2026-07-10 23:32:56 +0300

    Create history.js

---
+++ js/history.js
@@ -0,0 +1,104 @@
+// history.js
+// FashionAI Wear Memory System
+
+
+// ==========================
+// SAVE WORN OUTFIT
+// ==========================
+
+
+export function saveWornOutfit(
+database,
+outfit,
+occasion="Casual"
+){
+
+
+return new Promise((resolve,reject)=>{
+
+
+const transaction =
+database.transaction(
+"history",
+"readwrite"
+);
+
+
+
+const store =
+transaction.objectStore(
+"history"
+);
+
+
+
+const record = {
+
+
+date:
+new Date().toISOString(),
+
+
+
+occasion:occasion,
+
+
+
+outfit:{
+
+
+top:outfit.top.id,
+
+
+bottom:outfit.bottom.id,
+
+
+shoe:outfit.shoe.id
+
+
+},
+
+
+
+rating:null
+
+
+
+};
+
+
+
+
+
+const request =
+store.add(record);
+
+
+
+
+request.onsuccess=()=>{
+
+
+resolve(
+record
+);
+
+
+};
+
+
+
+request.onerror=()=>{
+
+
+reject(
+request.error
+);
+
+
+};
+
+
+});
+
+}
