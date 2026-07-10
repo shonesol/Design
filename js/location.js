63ac0c889318b2f1f417c5813ad5be0d42956b12
TAMALE SHONE <70106850+shonesol@users.noreply.github.com>
2026-07-11 02:30:46 +0300

    Create location.js

---
+++ js/location.js
@@ -0,0 +1,46 @@
+// FashionAI Location
+
+
+export function getUserLocation(){
+
+
+return new Promise(
+(resolve,reject)=>{
+
+
+navigator.geolocation.getCurrentPosition(
+
+(position)=>{
+
+
+resolve({
+
+latitude:
+position.coords.latitude,
+
+
+longitude:
+position.coords.longitude
+
+
+});
+
+
+},
+
+
+(error)=>{
+
+
+reject(error);
+
+
+}
+
+);
+
+
+});
+
+
+}
