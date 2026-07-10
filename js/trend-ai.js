320d96805550717088263b6d036e695255cc9861
TAMALE SHONE <70106850+shonesol@users.noreply.github.com>
2026-07-11 02:36:45 +0300

    Create trend-ai.js

---
+++ js/trend-ai.js
@@ -0,0 +1,70 @@
+// FashionAI Trend Intelligence
+
+
+export function analyzeTrends(){
+
+
+return {
+
+
+currentTrends:[
+
+
+"Quiet Luxury",
+
+"Old Money Style",
+
+"Minimalist Fashion",
+
+"Oversized Streetwear",
+
+"Sustainable Fashion",
+
+"Smart Casual"
+
+
+],
+
+
+
+popularColors:[
+
+
+"Black",
+
+"White",
+
+"Beige",
+
+"Navy",
+
+"Olive",
+
+"Chocolate Brown"
+
+
+],
+
+
+
+recommendedMaterials:[
+
+
+"Linen",
+
+"Cotton",
+
+"Denim",
+
+"Leather",
+
+"Wool"
+
+
+]
+
+
+};
+
+
+}
