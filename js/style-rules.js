9e9d74a11686ef4a83aaec43a08a7b55b277b4d1
TAMALE SHONE <70106850+shonesol@users.noreply.github.com>
2026-07-11 01:02:17 +0300

    Create style-rules.js

---
+++ js/style-rules.js
@@ -0,0 +1,49 @@
+// FashionAI Style Rules
+
+
+export const styleRules = {
+
+
+"Old Money":[
+
+"linen",
+"blazer",
+"loafers",
+"cream",
+"beige",
+"navy"
+
+],
+
+
+"Streetwear":[
+
+"hoodie",
+"sneakers",
+"cargo",
+"oversized"
+
+],
+
+
+"Business":[
+
+"shirt",
+"blazer",
+"trousers",
+"leather shoes"
+
+],
+
+
+"Traditional":[
+
+"kitenge",
+"kimono",
+"sari",
+"abaya"
+
+]
+
+
+};
