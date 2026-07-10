1aa27f436a2f0d2474a492ea108eed93f85bba8e
TAMALE SHONE <70106850+shonesol@users.noreply.github.com>
2026-07-11 02:14:43 +0300

    Create weather-ai.js

---
+++ js/weather-ai.js
@@ -0,0 +1,28 @@
+// FashionAI Weather Intelligence
+
+
+export async function getWeatherAdvice(){
+
+
+/*
+Later we connect a real weather API.
+
+For now we simulate weather data.
+*/
+
+
+return {
+
+
+condition:"Rainy",
+
+temperature:22,
+
+
+season:"Rainy Season"
+
+
+};
+
+
+}
