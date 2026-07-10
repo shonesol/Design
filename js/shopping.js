c26e7bb2c140b5a6063d14fdb51f272acd091733
TAMALE SHONE <70106850+shonesol@users.noreply.github.com>
2026-07-11 02:48:04 +0300

    Update shopping.js

--- js/shopping.js
+++ js/shopping.js
@@ -25,7 +25,159 @@ from "./shopping-ai.js";
 
 let database;
 
+import {
+auth
+}
+from "./firebase.js";
 
+
+import {
+onAuthStateChanged
+}
+from 
+"https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+
+import {
+openDatabase,
+getClothes
+}
+from "./db.js";
+
+
+import {
+analyzeWardrobeNeeds
+}
+from "./shopping-ai.js";
+
+
+
+let database;
+
+
+
+
+onAuthStateChanged(
+auth,
+async(user)=>{
+
+
+if(user){
+
+
+database =
+await openDatabase(
+user.uid
+);
+
+
+
+loadAdvice();
+
+
+}
+
+
+});
+
+
+
+
+
+
+
+async function loadAdvice(){
+
+
+
+const clothes =
+await getClothes(
+database
+);
+
+
+
+
+
+const advice =
+analyzeWardrobeNeeds(
+clothes
+);
+
+
+
+
+
+const box =
+document.getElementById(
+"shoppingResult"
+);
+
+
+
+
+
+box.innerHTML="";
+
+
+
+
+
+if(advice.length===0){
+
+
+box.innerHTML=
+
+`
+<h2>
+✅ Your wardrobe is balanced
+</h2>
+`;
+
+
+return;
+
+
+}
+
+
+
+
+
+
+advice.forEach(item=>{
+
+
+
+box.innerHTML += `
+
+
+<div class="card">
+
+
+<h2>
+🛒 ${item.item}
+</h2>
+
+
+<p>
+
+${item.reason}
+
+</p>
+
+
+</div>
+
+
+`;
+
+
+});
+
+
+
+}
 
 
 onAuthStateChanged(
