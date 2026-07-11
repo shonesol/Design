9a4def7548bc0d4707f529e8f4de2406048c93f9
TAMALE SHONE <70106850+shonesol@users.noreply.github.com>
2026-07-11 05:57:57 +0300

    Update location.js

--- js/location.js
+++ js/location.js
@@ -1,19 +1,39 @@
-// FashionAI Location
+// location.js
+// FashionAI User Location Intelligence
+
 
 
 export function getUserLocation(){
 
 
-return new Promise(
-(resolve,reject)=>{
+return new Promise((resolve,reject)=>{
+
+
+if(!navigator.geolocation){
+
+
+reject(
+"Location not supported"
+);
+
+
+return;
+
+
+}
+
+
+
 
 
 navigator.geolocation.getCurrentPosition(
+
 
 (position)=>{
 
 
 resolve({
+
 
 latitude:
 position.coords.latitude,
@@ -23,21 +43,43 @@ longitude:
 position.coords.longitude
 
 
+
 });
 
 
 },
 
 
+
 (error)=>{
+
+
+console.error(error);
+
 
 
 reject(error);
 
 
+},
+
+
+
+{
+
+
+enableHighAccuracy:true,
+
+
+timeout:10000
+
+
 }
 
+
+
 );
+
 
 
 });
