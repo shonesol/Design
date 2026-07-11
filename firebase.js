37877c3b1c3eb90449a4feb0ff132c96b3e15db3
TAMALE SHONE <70106850+shonesol@users.noreply.github.com>
2026-07-11 18:03:36 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,35 +1,24 @@
-import { initializeApp } 
-from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+// firebase.js
 
-import { getAuth } 
-from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 
-import { getFirestore } 
-from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-
-apiKey: "AIzaSyA1wmHCfPLOmw1u3zUM-M2bUlCUWJDv2Bk",
-
-authDomain: "design-a0e45.firebaseapp.com",
-
-projectId: "design-a0e45",
-
-storageBucket: "design-a0e45.appspot.com",
-
-messagingSenderId: "752963168855",
-
-appId: "1:752963168855:web:660513e16f91108e489112",
-
-measurementId: "G-3FYBPXQRPW"
-
+    apiKey: "YOUR_FIREBASE_API_KEY",
+    authDomain: "design-a0e45.firebaseapp.com",
+    projectId: "design-a0e45",
+    storageBucket: "design-a0e45.appspot.com",
+    messagingSenderId: "752963168855",
+    appId: "1:752963168855:web:660513e16f91108e489112"
 };
 
-
 const app = initializeApp(firebaseConfig);
-
 
 export const auth = getAuth(app);
 
 export const db = getFirestore(app);
+
+export default app;
