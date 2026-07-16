237b4cdcd4b81d2734d4b2f93041ea8aaa824fd5
TAMALE SHONE <70106850+shonesol@users.noreply.github.com>
2026-07-16 23:46:23 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -6,14 +6,7 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-aut
 
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-const firebaseConfig = {
-    apiKey: "YOUR_FIREBASE_API_KEY",
-    authDomain: "design-a0e45.firebaseapp.com",
-    projectId: "design-a0e45",
-    storageBucket: "design-a0e45.appspot.com",
-    messagingSenderId: "752963168855",
-    appId: "1:752963168855:web:660513e16f91108e489112"
-};
+
 
 const app = initializeApp(firebaseConfig);
 
